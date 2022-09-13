800f8fc2097513ecfb437e9a77df9f85881aff94
oppong-josephh <107093133+oppong-josephh@users.noreply.github.com>
2022-09-12 15:52:28 -1200

    react-ui assignment added

---
+++ React/react-ui/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Nav";
+import Body from "./components/Body";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
